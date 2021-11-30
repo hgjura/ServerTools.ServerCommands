5733f57f0e1791a0e48e90898e8c3d4768f43e55
Herald Gjura <hgjura@live.com>
2021-11-30 15:30:32 -0500

    initialize test suite

---
+++ ServerTools.ServerCommands.Tests/UnitTestsForServerCommands.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServerTools.ServerCommands.Tests
+{
+    [TestClass]
+    public class UnitTestsForServerCommands
+    {
+        private static CommandContainer _container;
+
+        /// <summary>
+        /// Execute once before the test-suite
+        /// </summary>
+        [ClassInitialize()]
+        public static void InitTestSuite(TestContext testContext)
+        {
+            _container = new CommandContainer();
+        }
+
+
+        [TestMethod]
+        public void A1010_TestInitializedContainerIsNotNull()
+        {
+            Assert.IsNotNull(_container);
+        }
+    }
+}
